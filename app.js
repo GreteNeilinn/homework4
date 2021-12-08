62cb52e3aa51c6d11d129b14c7be8f34fd9f34fb
HelgeLilleorg <helge.lilleorg@gmail.com>
2021-12-08 22:01:08 +0200

    header, footer, form started

--- app.js
+++ app.js
@@ -36,9 +36,43 @@ app.get('/:id', async(req, res) => {
 app.get('/singlepost', (req, res) => {
     res.render('singlepost', { title: 'Single post' });
 });
-app.get('/addnewpost', (req, res) => {
+/*app.get('/addnewpost', (req, res) => {
     res.render('addnewpost');
-});
+});*/
+
+app.post('/addnewpost', async(req, res) => {
+    try {
+    console.log("a post request has arrived");
+    const post = req.body;
+    // TIME
+    const time = now.getTime();
+    const newpost = await pool.query(
+    "INSERT INTO nodetable(title, body) values ($1, $2, $3) RETURNING*", [post.title, post.body, time]
+    );
+    res.json( newpost );
+    } catch (err) {
+    console.error(err.message);
+    }
+   });
+
+   $(function() {
+    $('#send').click(function() {
+    let title = $('form input').val()
+    let body = $('form textarea').val()
+    //create a Json object composed of these two values
+    let data = {
+    "title": title,
+    "body": body,
+    }
+
+    // instead of this I have to integrate here the POST request function (look above)
+    $.post('https://jsonplaceholder.typicode.com/posts', data,
+   function(response) {
+    //to show the object to be send we output the result to the console
+    console.log(response)
+    })
+    })
+   });
 
 app.use((req, res) => {
     res.status(404).render('404');
