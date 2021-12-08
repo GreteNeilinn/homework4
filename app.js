44d8ef040c904aea853e3bc502d2a82267aa4557
GreteNeilinn <grete.neilinn@gmail.com>
2021-12-08 23:11:14 +0200

    added css, fixed dp and post.ejs

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const pool = require('./database');
+const cors = require('cors');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.listen(3000);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log("Server is listening to port 3000")
+});
 
 
 app.get('/', async(req, res) => {
@@ -13,7 +22,7 @@ app.get('/', async(req, res) => {
         const posts = await pool.query(
             "SELECT * FROM poststable"
         );
-        res.json(posts.rows);
+        res.render('posts', { posts: posts.rows });
     } catch (err) {
         console.error(err.message);
     }
