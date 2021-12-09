455bbc39a1f496330656e215eb356276ec6e0831
GreteNeilinn <grete.neilinn@gmail.com>
2021-12-09 15:24:08 +0200

    footer and navbar added

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ app.listen(3000, () => {
     console.log("Server is listening to port 3000")
 });
 
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
-app.get('/', async(req, res) => {
+app.get('/posts', async(req, res) => {
     try {
         console.log("get posts request has arrived");
         const posts = await pool.query(
@@ -27,7 +30,6 @@ app.get('/', async(req, res) => {
         console.error(err.message);
     }
 });
-
 
 /*
 app.get('/:id', async(req, res) => {
@@ -42,46 +44,39 @@ app.get('/:id', async(req, res) => {
         console.error(err.message);
     }
 });
+
 */
 
 
-
-app.get('/singlepost', (req, res) => {
+app.get('/singlepost', async(req, res) => {
     try {
         const id = req.params.id;
         console.log(req.params.id);
         console.log("get a single post request has arrived");
         const posts = await pool.query(
-        "SELECT * FROM nodetable WHERE id = $1", [id]
+            "SELECT * FROM poststable WHERE id = $1", [id]
         );
         res.render('singlepost', { posts: posts.rows[0] });
-        } catch (err) {
+    } catch (err) {
         console.error(err.message);
-        }
+    }
 
-    });
+});
 
-
-
-
-    app.delete('/posts/:id', async(req, res) => {
-        try {
+app.delete('/posts/:id', async(req, res) => {
+    try {
         console.log(req.params);
         const { id } = req.params;
         const post = req.body;
         console.log("delete a post request has arrived");
         const deletepost = await pool.query(
-        "DELETE FROM nodetable WHERE id = $1", [id]
+            "DELETE FROM poststable WHERE id = $1", [id]
         );
         res.redirect('posts');
-        } catch (err) {
+    } catch (err) {
         console.error(err.message);
-        }
-       });
-       
-
-
-
+    }
+});
 
 
 app.get('/addnewpost', (req, res) => {
