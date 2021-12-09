fe7f11129f51647aeade91eb2957d1166dfd8777
GreteNeilinn <grete.neilinn@gmail.com>
2021-12-09 15:26:25 +0200

    likes added

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "postgres",
-    password: "",
+    password: "601031",
     database: "homework4",
     host: "localhost",
     port: "5432"
